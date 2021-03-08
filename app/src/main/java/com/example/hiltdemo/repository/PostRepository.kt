437910b1acb9b27716764488bc404c9457301232
AdemDev <gunayadem.dev@gmail.com>
2021-03-08 15:28:05 +0700

    Removed unused repo

--- app/src/main/java/com/example/hiltdemo/repository/PostRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.hiltdemo.repository
-
-//TODO Implement repository and add module for Hilt in `di/RepositoryModule' to get posts for data layer
-interface PostRepository {
-
-}
-
-class PostRepositoryImpl : PostRepository {
-}
